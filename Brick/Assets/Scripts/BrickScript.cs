f6be701612f9229ed3b3b687a78e79e56b62ef2d
xaa123aax <xaalibertieraax@gmail.com>
2019-07-15 11:35:48 +0800

    123

---
+++ Brick/Assets/Scripts/BrickScript.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickScript : MonoBehaviour
+{
+    public int hitsToKill;
+    public int points;
+    private int numberOfHits;
+     int starcolor;
+
+
+
+    void Start()
+    {
+        numberOfHits = 0;
+  
+
+    }
+    void SpawnStar()
+    {
+        GameObject newStar = Instantiate(Resources.Load<GameObject>("bonus"));
+        newStar.transform.position = this.gameObject.transform.position;
+
+    }
+    void SpawnStar2()
+    {
+        GameObject newStar2 = Instantiate(Resources.Load<GameObject>("bonus2"));
+        newStar2.transform.position = this.gameObject.transform.position;
+  
+    }
+    void SpawnStar3()
+    {
+        GameObject newStar3 = Instantiate(Resources.Load<GameObject>("bonus3"));
+        newStar3.transform.position = this.gameObject.transform.position;
+     
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+
+        if (other.gameObject.CompareTag("ball"))
+        {
+            numberOfHits++;
+
+            if (numberOfHits == hitsToKill)
+            {
+                // 破壞磚塊
+                this.gameObject.SetActive(false);
+                GameManager.playerPoints += points;
+                if (numberOfHits > 0)
+                {
+                    starcolor = Random.Range(1, 4);
+                    switch (starcolor)
+                    {
+                        case 1:
+                            SpawnStar();
+                            break;
+                        case 2:
+                            SpawnStar2();
+                            break;
+                        case 3:
+                            SpawnStar3();
+
+                            break;
+
+                        default:
+
+                            break;
+
+                    }
+
+                }
+            }
+
+        }
+
+    }
+
+}
+
+                   
---
+++ Brick/Assets/Scripts/CreateBrick.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateBrick : MonoBehaviour
+{
+    public int color;
+    public List<Transform> bricks;
+
+    void Start()
+    {
+        bricks = new List<Transform>();
+        for (int i = 0; i < 12; i++)
+        {
+            color = Random.Range(1, 5);
+            switch (color)
+            {
+                case 1:
+                    SpawnBrick();
+                    break;
+                case 2:
+                    SpawnBrick2();
+                    break;
+                case 3:
+                    SpawnBrick3();
+
+                    break;
+                case 4:
+                    SpawnBrick4();
+
+                    break;
+                default:
+
+                    break;
+
+            }
+        }
+    }
+    float BrickPositionX()
+    {
+        if (bricks.Count == 0)
+        {
+            return 0;
+        }
+        return Mathf.Floor(bricks.Count /4  )*3;
+    }
+    float BrickPositionY()
+    {
+        if (bricks.Count == 0)
+        {
+            return 0;
+        }
+        return bricks.Count % 4;
+    }
+
+    void SpawnBrick()
+    {
+        GameObject newBrick = Instantiate(Resources.Load<GameObject>("redbrick"));
+        newBrick.transform.position = new Vector2(BrickPositionX(), BrickPositionY());
+        bricks.Add(newBrick.transform);
+
+    }
+    void SpawnBrick2()
+    {
+        GameObject newBrick2 = Instantiate(Resources.Load<GameObject>("yellowbrick"));
+        newBrick2.transform.position = new Vector2(BrickPositionX(), BrickPositionY());
+        bricks.Add(newBrick2.transform);
+
+    }
+    void SpawnBrick3()
+    {
+        GameObject newBrick3 = Instantiate(Resources.Load<GameObject>("greenbrick"));
+        newBrick3.transform.position = new Vector2(BrickPositionX(), BrickPositionY());
+        bricks.Add(newBrick3.transform);
+
+    }
+    void SpawnBrick4()
+    {
+        GameObject newBrick4 = Instantiate(Resources.Load<GameObject>("bluebrick"));
+        newBrick4.transform.position = new Vector2(BrickPositionX(), BrickPositionY());
+        bricks.Add(newBrick4.transform);
+    }
+
+    void Update()
+    {
+
+    }
+
+}
